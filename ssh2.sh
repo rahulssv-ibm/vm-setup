a948948bbe4642372a47414cfca18e14b5db54df
rahulssv-ibm <rahul.vishwakarma2@ibm.com>
2024-09-30 22:22:25 +0530

    update

--- ssh2.sh
+++ ssh2.sh
@@ -8,6 +8,9 @@ git clone https://github.com/rahulssv-ibm/gaplib.git
 cd gaplib/build-files
 cat lxd-preseed.yaml | lxd init --preseed
 lxc storage set default volume.block.filesystem xfs
+lxc network set lxdbr0 ipv6.firewall false
+lxc network set lxdbr0 ipv4.firewall false
+sudo systemctl reload snap.lxd.daemon
 
 cd ..
 ./setup-build-env.sh -s 8
@@ -16,11 +19,13 @@ cd ..
 git clone https://${GE_TOKEN}@github.ibm.com/ppc64le-automation/actions-runner.git
 # setup lxd
 # Change directory to the actions-runner directory in the home directory
-cd actions-runner || exit
+cd actions-runner
+git checkout sso-flow 
 # Pull the latest changes from Git
-git pull || exit
+git pull 
 
 # Build the 'lxd' service quietly
-docker compose build -q lxd || exit
+sudo systemctl restart docker.socket
+docker compose build -q lxd 
 # Start the 'lxd' service in detached mode using the prod-compose.yml file
-docker compose -f prod-compose.yml up -d lxd || exit
+docker compose -f prod-compose.yml up -d lxd 
