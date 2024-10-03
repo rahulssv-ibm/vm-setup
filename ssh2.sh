68745f31d37b5cf2144793519beeebf30e6e7ec0
rahulssv-ibm <rahul.vishwakarma2@ibm.com>
2024-10-03 11:16:25 +0530

    update

--- ssh2.sh
+++ ssh2.sh
@@ -9,8 +9,9 @@ git clone https://github.com/rahulssv-ibm/gaplib.git
 cd gaplib/build-files
 cat lxd-preseed.yaml | lxd init --preseed
 lxc storage set default volume.block.filesystem xfs
-lxc network set lxdbr0 ipv6.firewall false
-lxc network set lxdbr0 ipv4.firewall false
+for ipt in iptables ip6tables; do $ipt --flush; $ipt --flush -t nat; $ipt --flush -t mangle; $ipt --delete-chain; $ipt --delete-chain -t nat; $ipt -P FORWARD ACCEPT; $ipt -P INPUT ACCEPT; $ipt -P OUTPUT ACCEPT; done
+# lxc network set lxdbr0 ipv6.firewall false
+# lxc network set lxdbr0 ipv4.firewall false
 sudo systemctl reload snap.lxd.daemon
 sleep 10
 cd ..
